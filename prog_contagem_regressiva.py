6ad995b97c6c47ef23654ff4f4e234dee6f0cfca
Isferbr <89714782+Isferbr@users.noreply.github.com>
2021-08-29 16:50:36 -0300

    Add files via upload
    Código em Python para um contador regressivo.

---
+++ prog_contagem_regressiva.py
@@ -0,0 +1,3 @@
+# Faça um programa para escrever a contagem regressiva do lançamento de um foguete. O programa deve imprimir “10, 9, 8, ..., 1 e Fogo!” na tela. OBS: utilize uma estrutura de repetição (for ou while) na sua resposta.
+for x in range(10,-1,-1):
+    print(x)
